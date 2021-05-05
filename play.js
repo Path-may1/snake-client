250dc05a955346faec07cee5d026fc91ceb34408
Path-may1 <80159654+Path-may1@users.noreply.github.com>
2021-05-05 19:46:40 +0000

    input.js/play.js

--- play.js
+++ play.js
@@ -1,25 +1,31 @@
 const connect = require("./client");
 
 
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
+setupInput();
+console.log("Connecting ...");
+connect();
+
+// const setupInput = function () {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding("utf8");
+//   stdin.resume();
+//   stdin.on("data", handleUserInput);
+//   return stdin;
   
-};
+// };
 
 
-const handleUserInput = function (input) {
- if(input === '\u0003'){
-   process.exit()
- }
-};
+// const handleUserInput = function (input) {
+//  if(input === '\u0003'){
+//    process.exit()
+//  }
+// };
 
 module.exports = setupInput
