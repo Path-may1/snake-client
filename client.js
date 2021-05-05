299673dfc533db2f46ed093f830a6588a0161197
Path-may1 <80159654+Path-may1@users.noreply.github.com>
2021-05-05 19:16:45 +0000

    play.js

--- client.js
+++ client.js
@@ -4,16 +4,30 @@ const connect = function () {
     host: '135.23.222.131',
     port: 50542
   });
-
+const name = "May"
   // interpret incoming data as text
   conn.setEncoding("utf8");
   
   conn.on("connect", () => {
     // code that does something when the connection is first established
+    conn.write('successfully connected to game server');
+  });
+
+  conn.on('connect', () => {
     conn.write('Hello from client!');
+   // conn.write("Move; up");
+       setInterval (() => {
+    conn.write("Move: up");
+    }, 200);
+   
+  });
+  
+
+
+  conn.on('data', (data) => {
+    console.log(data);
   });
   return conn;
-
 }
 module.exports = connect;
 
--- play.js
+++ play.js
@@ -1,23 +1,6 @@
 const connect = require("./client");
-//const net = require("net");
 
-// establishes a connection with the game server
-// const connect = function () {
-//   const conn = net.createConnection({
-//     host: '135.23.222.131',
-//     port: 50542
-//   });
 
-//   // interpret incoming data as text
-//   conn.setEncoding("utf8");
-  
-//   conn.on("connect", () => {
-//     // code that does something when the connection is first established
-//     conn.write('Hello from client!');
-//   });
-//   return conn;
-
-// }
 
 console.log("Connecting ...");
 connect();
